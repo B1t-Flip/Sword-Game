ded537f3890b73c2e967bbd66fcc026e5edc1e1e
Vlad Ciobanu <jumiciobi@gmail.com>
2026-03-04 12:07:58 +0000

    some debugging

--- Assets/Code/Props/WinLevel.cs
+++ Assets/Code/Props/WinLevel.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class WinLevel : MonoBehaviour {
+  [SerializeField] private GameObject winScreen;
   private void OnTriggerEnter2D(Collider2D other) {
-    if (other.CompareTag("Player")) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    if (!other.CompareTag("Player")) return;
+    if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1)
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    else winScreen.SetActive(true);
   }
 }
